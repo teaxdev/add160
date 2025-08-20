67a1d976e4053fa074bc781692591391871dd815
Alex <FloresAlex6904@gmail.com>
2025-08-19 20:04:22 -0500

    continue oop assignment

--- bookoop.py
+++ bookoop.py
@@ -13,6 +13,9 @@ book2 = Book("Percy Jackson, The Sea Of Monsters", "Rick Riordan", "279")
 book3 = Book("Percy Jackson, The Titan's Curse", "Rick Riordan", "213")
 
 def display_details():
+    for i in Book:
+        print(f"")
+        i += 1
     print(f"Title: {book1.title}, Author: {book1.author}, Pages: {book1.pages}")
     print(book2.title, book2.author, book2.pages)
     print(book3.title, book3.author, book3.pages)
