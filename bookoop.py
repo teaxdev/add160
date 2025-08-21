6c1e2b14773a8c934ef1257ce38f0d3bb170038b
teaxdev <FloresAlex6904@gmail.com>
2025-08-21 11:23:18 -0500

    finish bookoop.py

--- bookoop.py
+++ bookoop.py
@@ -1,5 +1,5 @@
 # Book Object Oriented Programming Assignment
-class Book():
+class Book(): # Defining the book class
     def __init__(self, title, author, pages):
         self.title = title
         self.author = author
@@ -9,34 +9,36 @@ book1 = Book("Percy Jackson, The Lightning Thief", "Rick Riordan", "377")
 book2 = Book("Percy Jackson, The Sea Of Monsters", "Rick Riordan", "279")
 book3 = Book("Percy Jackson, The Titan's Curse", "Rick Riordan", "213")
 
-tup = (book1, book2, book3)
-def display_details():
-    count = 1
-    if count == 1:
+def display_details(x): # Cycles through each book and prints its details
+    if x == 1:
         print(f"Title: {book1.title}, Author: {book1.author}, Pages: {book1.pages}")
-        count += 1
-        return count
-    elif count == 2:
+    elif x == 2:
         print(f"Title: {book2.title}, Author: {book2.author}, Pages: {book2.pages}")
-        count += 1
-        return count
     else:
         print(f"Title: {book3.title}, Author: {book3.author}, Pages: {book3.pages}")
-    
-    
-display = display_details()
 
-def is_long_book():
-    if int(book1.pages) >= 100:
-        print("True")
-    else:
-        print("False")
+def is_long_book(x): # Prints whether each book is more than 100 pages
+    if x == 1:
+        if int(book1.pages) >= 100:
+            print("True")
+        else:
+            print("False")
+    if x == 2:
+        if int(book2.pages) >= 100:
+            print("True")
+        else:
+            print("False")
+    elif x == 3:
+        if int(book3.pages) >= 100:
+            print("True")
+        else:
+            print("False")
 
-def main():
+def main(): # Loops through each function 3 times
     i = 1
     while i <= 3:
-        display_details()
-        is_long_book()
+        display_details(x=i)
+        is_long_book(x=i)
         i += 1
 
 main()
