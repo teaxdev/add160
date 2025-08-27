af4ce0181a1272c1bb974b4948f2c56ece6d1dad
Alex <FloresAlex6904@gmail.com>
2025-08-26 19:39:09 -0500

    start addressbook.py

---
+++ m2/addressbook.py
@@ -0,0 +1,16 @@
+# OOP 2.1 Core Syntax Assignment
+
+class AddressBook():
+    def __init__(self, first_name, last_name, birthday, email,
+                   street_address, city, state, zip, phone):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birthday = birthday
+        self.email = email
+        self.street_address = street_address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.phone = phone
+
+    
