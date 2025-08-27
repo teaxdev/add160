5f9be7d83cb0f8c1fd9196cbe92b5dc0b954e653
Alex <FloresAlex6904@gmail.com>
2025-08-26 20:25:50 -0500

    continue addressbook.py

--- m2/addressbook.py
+++ m2/addressbook.py
@@ -1,6 +1,25 @@
 # OOP 2.1 Core Syntax Assignment
 
 class AddressBook():
+    """
+    A class that represents an Address Book
+
+    Attributes:
+    first_name: str -- the users first name
+    last_name: str -- the users last name
+    birthday: str -- the users date of birth
+    email: str -- the users email address
+    street_address: str -- the users street address
+    city: str -- the city where the user lives
+    state: str -- the state where the user lives
+    zip: int -- the users zip code
+    phone: int -- the users phone number
+
+    Methods:
+    __str__
+    __repr__
+    __eq__
+    """
     def __init__(self, first_name, last_name, birthday, email,
                    street_address, city, state, zip, phone):
         self.first_name = first_name
@@ -13,4 +32,11 @@ class AddressBook():
         self.zip = zip
         self.phone = phone
 
-    
+    def __str__(self):
+        print()
+
+    def __repr__(self):
+        print()
+
+    def __eq__(self):
+        print()
