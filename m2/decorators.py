8cd42902ef78bce3fa626de902a852d8f82b0253
teaxdev <FloresAlex6904@gmail.com>
2025-09-13 14:10:45 -0500

    finish decorators.py

--- m2/decorators.py
+++ m2/decorators.py
@@ -1,22 +1,20 @@
 # OOP 2.4 Decorators
 
+import time, math
 class TimingDecorator():
-    def __init__(self, func, start_time, end_time):
+    def __init__(self, func):
         self.func = func
-        self.start_time = start_time
-        self.end_time = end_time
-
     def __call__(self, *args, **kwargs):
-        start_time = self.start_time.time() 
+        start_time = time.time() 
         call_func = self.func(*args, **kwargs)
-        end_time = self.end_time.time()
-        print(f"Function {self.func.__name__} took {end_time - start_time:.4f} seconds to execute")
+        end_time = time.time()
+        print(f"Execution time: {end_time - start_time:.4f} seconds")
         return call_func
         # result = call_func
 
 @TimingDecorator
 def compute(x):
-    y = x^132
-    print(y)
+    y = math.factorial(x)
+    print(f"Output: {y}")
 
-compute()
+compute(123)
