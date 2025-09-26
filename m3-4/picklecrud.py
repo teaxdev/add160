c5dec90c39953c50bd4ccf1feeb823c0ff9cfe9c
teaxdev <FloresAlex6904@gmail.com>
2025-09-26 13:27:57 -0500

    start pickle

---
+++ m3-4/picklecrud.py
@@ -0,0 +1,15 @@
+# OOP 4.2 Serialization with pickles
+
+book = {"First Name": "", "Last Name": "", "Number": "", "Email": ""}
+x = 1
+def choice():
+    if x == "1":
+        print("goto 1")
+    if x == "2":
+        print("goto 2")
+    if x == "3":
+        print("goto 3")
+    if x == "4":
+        print("goto 4")
+    else:
+        print("exit")
